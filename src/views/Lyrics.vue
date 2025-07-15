3cff46c73ae5f45b6487b4758200a2b510177fa2
LonelySail <xiaocaicai_email@sina.com>
2025-07-15 19:35:22 +0800

    fix:桌面歌词切换不及时。

--- src/views/Lyrics.vue
+++ src/views/Lyrics.vue
@@ -207,7 +207,7 @@ const updateDisplayedLines = () => {
         if (currentIdx % 2) displayedLines.value = [currentIdx + 1, currentIdx]
         else displayedLines.value = [currentIdx, currentIdx + 1]
         currentLineScrollX.value = 0
-    }, 500)
+    }, 200)
 }
 
 // 开始拖动
@@ -237,7 +237,7 @@ const checkMousePosition = (event) => {
 }
 
 window.electron.ipcRenderer.on('lyrics-data', (data) => {
-    if (data.currentTime < 1 || lyrics.value.length === 0 || data.currentSongHash !== currentSongHash) {
+    if (data.currentTime < 1 || data.lyricsData.length === 0 || data.currentSongHash !== currentSongHash) {
         currentSongHash = data.currentSongHash
         lyrics.value = data.lyricsData;
         currentLineIndex.value = 0;
